f0f71b3f24c46533f4d07b0987dedc482cc859c0
comame <dev@comame.xyz>
2025-05-11 18:04:15 +0900

    リプライツリーを特定

--- lib/discord_gemini_bot/discord.rb
+++ lib/discord_gemini_bot/discord.rb
@@ -24,8 +24,13 @@ module Discord
         next unless bot_mentioned || replied_message.user.id == bot.profile.id
       end
 
+      reply_id = reply_tree_id bot, message_event.message
+
       message_event.message.channel.start_typing
-      message_event.respond 'hi!', false, nil, nil, nil, message_event.message, nil
+      sent_message = message_event.respond "リプライツリー: #{reply_id}", false, nil, nil, nil, message_event.message, nil
+
+      @replay_tree_cache ||= {}
+      @replay_tree_cache[sent_message.id] = reply_id
     end
 
     bot.run true
@@ -33,6 +38,34 @@ module Discord
     bot.online
 
     bot.join
+  end
+
+  # リプライツリーを特定する ID を返す
+  def reply_tree_id(bot, message)
+    follow_reply = true
+
+    loop do
+      unless message.referenced_message.nil?
+        message = message.referenced_message
+        next if follow_reply
+      end
+
+      # リプライツリーは1回だけ辿る
+      follow_reply = false
+
+      # bot自身の投稿なら、その投稿をキーにしてリプライツリーを特定する
+      if message.author.id == bot.profile.id
+        @replay_tree_cache ||= {}
+        id = @replay_tree_cache[message.id]
+        return id unless id.nil?
+
+        # 紐づいたリプライツリーが見つからなければ、しょうがないので新しいリプライツリーということにしておく
+        return message.id
+      end
+
+      # 返信ではなかった場合、新しいリプライツリー
+      return message.id
+    end
   end
 
   # DISCORD_BOT_TOKENを取得
